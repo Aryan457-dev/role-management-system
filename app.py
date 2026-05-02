20826974606b31fa7deb216051331814b11f1424
Aryan Dabholkar <aryandabholkar@Aryans-MacBook-Air.local>
2026-05-02 20:03:22 +0530

    Ready for deployment

--- app.py
+++ app.py
@@ -79,5 +79,8 @@ def delete(id):
 
     return redirect('/')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
